a01967cc96dc7b755d0ff354a327ef99039ceccd
Harry Chen <harrchen@microsoft.com>
2016-11-12 21:50:12 -0500

    Added comments

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
+    //azure mongo db service
     mongoUrl: 'mongodb://mongdbasservice1:PXuwRzskvaE5NPhW9Z4DGKKO7Xvppu3B60jjLEywG3l2CThn0nXHwPPJb8TF0qYhOocnwEmckRbJaVezbxnEzA==@mongdbasservice1.documents.azure.com:10250/?ssl=true'
 };
