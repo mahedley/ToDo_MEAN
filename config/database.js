9d155a2bdde4bb86a61f61dd280cee679d8d6e2f
mahedley <mahedley@microsoft.com>
2017-04-27 00:02:32 -0700

    commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //azure mongo db service. Below is just a place holder value. Replace with your own url
     //check https://azure.microsoft.com/en-us/documentation/articles/documentdb-protocol-mongodb/ for details
-    mongoUrl: 'mongodb://youradminname:youradminpassword@10.0.0.10:27017,10.0.0.11:27017/tasks?replicaSet=rs0'
+    mongoUrl: 'mongodb://mdh:B1gP@ssw0rd!@10.0.0.10:27017,10.0.0.11:27017/tasks?replicaSet=rs0'
 };
