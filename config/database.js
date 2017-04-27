890c92ec9baf147d4ee4432f418795cafa6519e9
mahedley <mahedley@microsoft.com>
2017-04-26 18:32:16 -0700

    database path

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //azure mongo db service. Below is just a place holder value. Replace with your own url
     //check https://azure.microsoft.com/en-us/documentation/articles/documentdb-protocol-mongodb/ for details
-    mongoUrl: 'mongodb://docdbasmongo:bYfmwUuPhwZhnK9GqJ16sCLhwZghZ8Tb1rOIDSj482qbvHuUQ2wlj4P9fnydsGhVlSiMb2OcgM18zbNaMrGxNw==@docdbasmongo.documents.azure.com:10250/?ssl=true'
+    mongoUrl: 'mongodb://mongo'
 };
